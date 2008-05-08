49b52e3697c5963b43fa7df8a1ab14d6005f2ad5
Thoronador <thoronador@users.sourceforge.net>
2008-05-08 11:50:50 +0000

    created trunk directory + first units for terrain, goods and units

---
+++ engine/Goods.pas
@@ -0,0 +1,12 @@
+unit Goods;
+
+interface
+
+type
+ TGoodType = (gtFood, gtSugar, gtTobacco, gtCotton, gtFur, gtWood, gtOre,
+              gtSilver, gtHorses, gtRum, gtCigar, gtCloth, gtCoat, gtTradegoods,
+              gtTool, gtMusket, gtHammer, gtLibertyBell);
+              
+implementation
+
+end.
