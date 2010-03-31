a37a74000ac34af1e8b0f39c907fd7250cbb7a9e
Thoronador <thoronador@users.sourceforge.net>
2010-03-31 04:04:48 +0000

    Starting transition from Object Pascal to C++.

---
+++ engine/Goods.h
@@ -0,0 +1,69 @@
+#ifndef GOODS_H
+#define GOODS_H
+
+enum TGoodType {gtFood, gtSugar, gtTobacco, gtCotton, gtFur, gtWood, gtOre,
+                gtSilver, gtHorses, gtRum, gtCigar, gtCloth, gtCoat, gtTradegoods,
+                gtTool, gtMusket, gtHammer, gtLibertyBell, gtCross};
+
+struct GoodData
+{
+  unsigned char diff;
+  unsigned char start_min, start_max;
+  unsigned char min, max;
+};
+
+const GoodData cGoodPrices[] = {
+             {diff: 8, start_min: 1, start_max: 3, min: 1, max: 6},//gtFood
+             {diff: 2, start_min: 4, start_max: 7, min: 3, max: 7},//gtSugar
+             {diff: 2, start_min: 3, start_max: 5, min: 2, max: 5},//gtTobacco
+             {diff: 2, start_min: 2, start_max: 5, min: 2, max: 5},//gtCotton
+             {diff: 2, start_min: 4, start_max: 6, min: 2, max: 6},//gtFur
+             {diff: 5, start_min: 2, start_max: 2, min: 2, max: 2},//gtWood
+             {diff: 3, start_min: 3, start_max: 6, min: 2, max: 6},//gtOre
+             {diff: 1, start_min: 20, start_max: 20, min: 2, max: 20},//gtSilver
+             {diff: 1, start_min: 2, start_max: 3, min: 2, max: 11},//gtHorses
+             {diff: 1, start_min: 11, start_max: 13, min: 1, max: 20},//gtRum
+             {diff: 1, start_min: 11, start_max: 13, min: 1, max: 20},//gtCigar
+             {diff: 1, start_min: 11, start_max: 13, min: 1, max: 20},//gtCloth
+             {diff: 1, start_min: 11, start_max: 13, min: 1, max: 20},//gtCoat
+             {diff: 1, start_min: 2, start_max: 3, min: 2, max: 12},//gtTradegoods
+             {diff: 1, start_min: 2, start_max: 2, min: 2, max: 9},//gtTool
+             {diff: 1, start_min: 3, start_max: 3, min: 2, max: 20},//gtMusket
+             {diff: 0, start_min: 0, start_max: 0, min: 0, max: 0},//gtHammer
+             {diff: 0, start_min: 0, start_max: 0, min: 0, max: 0},//gtLibertyBell
+             {diff: 0, start_min: 0, start_max: 0, min: 0, max: 0}//gtCross
+           };
+
+
+
+/*
+ cGoodPrices: array [TGoodType] of record
+                 diff: Byte;
+                 start_min, start_max: Byte;
+                 min, max: Byte;
+               end =(
+             (diff: 8; start_min: 1; start_max: 3; min: 1; max: 6),//gtFood
+             (diff: 2; start_min: 4; start_max: 7; min: 3; max: 7),//gtSugar
+             (diff: 2; start_min: 3; start_max: 5; min: 2; max: 5),//gtTobacco
+             (diff: 2; start_min: 2; start_max: 5; min: 2; max: 5),//gtCotton
+             (diff: 2; start_min: 4; start_max: 6; min: 2; max: 6),//gtFur
+             (diff: 5; start_min: 2; start_max: 2; min: 2; max: 2),//gtWood
+             (diff: 3; start_min: 3; start_max: 6; min: 2; max: 6),//gtOre
+             (diff: 1; start_min: 20; start_max: 20; min: 2; max: 20),//gtSilver
+             (diff: 1; start_min: 2; start_max: 3; min: 2; max: 11),//gtHorses
+             (diff: 1; start_min: 11; start_max: 13; min: 1; max: 20),//gtRum
+             (diff: 1; start_min: 11; start_max: 13; min: 1; max: 20),//gtCigar
+             (diff: 1; start_min: 11; start_max: 13; min: 1; max: 20),//gtCloth
+             (diff: 1; start_min: 11; start_max: 13; min: 1; max: 20),//gtCoat
+             (diff: 1; start_min: 2; start_max: 3; min: 2; max: 12),//gtTradegoods
+             (diff: 1; start_min: 2; start_max: 2; min: 2; max: 9),//gtTool
+             (diff: 1; start_min: 3; start_max: 3; min: 2; max: 20),//gtMusket
+             (diff: 0; start_min: 0; start_max: 0; min: 0; max: 0),//gtHammer
+             (diff: 0; start_min: 0; start_max: 0; min: 0; max: 0),//gtLibertyBell
+             (diff: 0; start_min: 0; start_max: 0; min: 0; max: 0)//gtCross
+           ); */
+
+
+
+#endif // GOODS_H
+
---
+++ engine/Terrain.h
@@ -0,0 +1,93 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include "Goods.h"
+const unsigned char TERRAIN_RIVER_BIT = 1;
+const unsigned char TERRAIN_ROAD_BIT = 2;
+const unsigned char TERRAIN_SPECIAL_BIT = 4;
+const unsigned char TERRAIN_PLOUGHED_BIT = 8;
+
+enum TTerrainType {//open terrain types
+                  ttArctic, ttSea, ttOpenSea, ttPlains, ttGrassland, ttPrairie,
+                  ttSavannah, ttMarsh, ttSwamp, ttDesert, ttTundra,
+                  //forested terrain types
+                  ttBoreal, ttWetland, ttScrubForest, ttBroadleaf, ttMixedForest,
+                  ttConiferForest, ttRainForest, ttTropicalForest,
+                  //others
+                  ttHills, ttMountains};
+
+class TTerrain
+{
+  private:
+    bool m_River;
+    bool m_Road;
+
+      /**indicates, whether terrain has a special ressource.
+        arctic: none
+        Sea, OpenSea: Fish
+        Plains: Wheat
+        Grassland: Tobacco
+        Prairie: Cotton
+        Savannah: Sugar
+        Marsh: Minerals
+        Swamp: Minerals
+        Desert: Oasis
+        Tundra: None allowed
+        Boreal: Wild/ Deer
+        Wetland: Minerals
+        ScrubForest: Oasis
+        Broadleaf: Wild/Deer
+        MixedForest: Beaver
+        ConiferForest: Best Wood
+        RainForest: Minerals
+        TropicalForest: BestWood
+        Hills: Ore
+        Mountains: Silver
+       **/
+    bool m_Special;
+    bool m_Ploughed;
+
+  public:
+    TTerrainType m_Type;
+
+    TTerrain(const TTerrainType ATerrain, const bool River=false,
+            const bool Road=false, const bool Special=false,
+            const bool Ploughed=false);
+    ~TTerrain();
+
+
+    TTerrainType GetType();
+    TTerrainType ClearedBecomes();
+    bool HasForest();
+    bool HasRiver();
+    bool HasRoad();
+    bool HasSpecial();
+    bool IsPloughed();
+    bool IsWater();
+
+    unsigned char GetGoodProduction(const TGoodType AGood, const bool expert);
+    //for colony base fields
+    unsigned char GetColonyFood();
+    TGoodType GetColonyGoodType();
+    unsigned char GetColonyGoodAmount();
+    //defense bonus for terrain, in percent. Maximum is 150, so Byte will do.
+    unsigned char GetDefenceBonus();
+
+    //change terrain state
+    void ClearForest();
+    void CreateRoad();
+    void Plough();
+    //changes that should only take place at initialization
+    void CreateSpecial();
+};//class
+
+const TTerrainType ttFlachland = ttPlains;
+const TTerrainType ttGreenland = ttGrassland;
+  //ttFeuchtgebiete = ttMarsh;
+  //ttGestrueppwald = ttScrub;
+  //ttNadelwald = ttConifer;
+  //ttLaubwald = ttBroadleafForest;
+  //ttFeuchtwald = ttWetland;
+
+#endif
+
