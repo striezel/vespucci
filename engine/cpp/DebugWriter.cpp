af4ad09e6b3c942ce64366719f1cc2245cfbf03c
Dirk Stolle <striezel-dev@web.de>
2015-02-07 18:16:41 +0100

    transition: add C++ equivalent of DebugWriter.pas

---
+++ engine/cpp/DebugWriter.cpp
@@ -0,0 +1,35 @@
+/* **************************************************************************
+
+    This file is part of Vespucci.
+    Copyright (C) 2015  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ***************************************************************************
+*/
+
+//change the following line to enable debug mode
+//PlaceHashSignHereDEFINE DEBUG_CODE 1
+
+#include "DebugWriter.hpp"
+#ifdef DEBUG_CODE
+#include <iostream>
+#endif // DEBUG_CODE
+
+void WriteDebugLn(const std::string& message)
+{
+  #ifdef DEBUG_CODE
+  std::cout << message << "\n";
+  #endif // DEBUG_CODE
+}
